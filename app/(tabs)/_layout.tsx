b60d5fc3d02be2f58f68c9a0c54bf971434de31b
anjelica <144828225+anjelica0224@users.noreply.github.com>
2025-11-02 14:23:08 +0530

    feat: initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,16 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Octicons from '@expo/vector-icons/Octicons';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
+export default function _Layout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name="index" options={{ headerShown: true, tabBarIcon: ({color, focused}) => <Feather name={focused ? "home" : "home-outline"} size={24} color={color} />, tabBarShowLabel: false, tabBarActiveTintColor: '#ffd33d'}} />
+      <Tabs.Screen name="explore" options={{ headerShown: false, tabBarIcon: ({focused}) => <FontAwesome5 name="wpexplorer" size={24}/>, tabBarShowLabel: false}} />
+      <Tabs.Screen name="profile" options={{ headerShown: false, tabBarIcon: ({focused}) => <Octicons name="person" size={24}/>, tabBarShowLabel: false}} />
     </Tabs>
   );
 }
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Profile() {
   return (
     <View className ="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold text-blue-400">celestia.</Text>
+      <Text className="text-2xl font-bold text-blue-400">eg f</Text>
     </View>
   );
 }
